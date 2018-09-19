34c279be868863882d70ab32b8ef8b162adb969f
Alahari <268677@cognizant.com>
2018-09-18 21:23:39 -0400

    Added NgModel to read the input data and pass to next screens.

--- src/pages/event-details/event-details.ts
+++ src/pages/event-details/event-details.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+import { EventInvitePage } from '../event-invite/event-invite';
 
 /**
  * Generated class for the EventDetailsPage page.
@@ -14,7 +17,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'event-details.html',
 })
 export class EventDetailsPage {
-
+  public eventType:any;
+  public hostedBy:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -22,4 +26,9 @@ export class EventDetailsPage {
     console.log('ionViewDidLoad EventDetailsPage');
   }
 
+  continue(){
+      console.log('Invoking Next Page :: '+this.eventType+" hosted by :: "+this.hostedBy);
+      alert("Event Name "+this.eventType+" hosted by "+this.hostedBy)
+      this.navCtrl.push(EventInvitePage,{eventType:this.eventType,hostedBy:this.hostedBy});
+    }
 }
--- src/pages/event-invite/event-invite.ts
+++ src/pages/event-invite/event-invite.ts
@@ -14,12 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'event-invite.html',
 })
 export class EventInvitePage {
-
+  public userName:any;
+  public eventType:any;
+  public hostedBy:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.eventType = this.navParams.get("eventType");
+    this.hostedBy = this.navParams.get("hostedBy");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventInvitePage');
   }
 
+  login(){
+    console.log('in the log in method :: '+this.userName);
+    console.log('Invoking Next Page :: '+this.eventType+" hosted by :: "+this.hostedBy);
+    if(this.userName == ""){
+      alert("Please enter user name");
+    }
+  }
 }
